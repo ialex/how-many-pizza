fe2d6c5de6bb6b0ae92e7486a7c2ee784b050dbd
Studs <a.j.studnicky@gmail.com>
2018-10-29 18:15:02 -0400

    Implemented better pizza coverage

--- test.js
+++ test.js
@@ -7,19 +7,19 @@ assert.strictEqual(howManyPizza(null), 0);
 assert.strictEqual(howManyPizza(undefined), 0);
 assert.strictEqual(howManyPizza(50), 19);
 assert.strictEqual(howManyPizza(1.5), 1);
-assert.strictEqual(howManyPizza(5.5), 2);
+assert.strictEqual(howManyPizza(5.5), 3);
 
 assert.strictEqual(howManyPizza(null, null), 0);
 assert.strictEqual(howManyPizza(undefined, undefined), 0);
 assert.strictEqual(howManyPizza(50, 3), 19);
 assert.strictEqual(howManyPizza(1.5, 3), 1);
-assert.strictEqual(howManyPizza(5.5, 3), 2);
+assert.strictEqual(howManyPizza(5.5, 3), 3);
 
 assert.strictEqual(howManyPizza(null, null, null), 0);
 assert.strictEqual(howManyPizza(undefined, undefined, undefined), 0);
 assert.strictEqual(howManyPizza(50, 3, 8), 19);
 assert.strictEqual(howManyPizza(1.5, 3, 8), 1);
-assert.strictEqual(howManyPizza(5.5, 3, 8), 2);
+assert.strictEqual(howManyPizza(5.5, 3, 8), 3);
 
 assert.strictEqual(howManyPizza(50, 4, 12), 17);
 
